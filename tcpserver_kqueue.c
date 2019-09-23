45bdc148b2d35911d869f24f4239a366e0bf3343
Hielke de Vries <hdevries@fastmail.com>
2019-09-23 17:32:01 +0200

    Improve comments.

--- tcpserver_kqueue.c
+++ tcpserver_kqueue.c
@@ -8,7 +8,7 @@
 
 int main()
 {
-    // All variables needed
+    // All needed variables.
     int socket_listen_fd,
         portno = 1815,
         client_len,
@@ -20,14 +20,14 @@ int main()
     struct sockaddr_in serv_addr,
         client_addr;
 
-    // Create socket
+    // Create socket.
     if (((socket_listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0))
     {
         perror("ERROR opening socket");
         exit(1);
     }
 
-    // Create socket structure and bind to ip address
+    // Create socket structure and bind to ip address.
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -39,24 +39,28 @@ int main()
         exit(1);
     }
 
-    // Start listening
+    // Start listening.
     listen(socket_listen_fd, 3);
     client_len = sizeof(client_addr);
 
-    // Prepare the kqueue
+    // Prepare the kqueue.
     kq = kqueue();
 
-    // Create event
+    // Create event 'filter', these are the events we want to monitor.
+    // Here we want to monitor: socket_listen_fd, for the events: EVFILT_READ 
+    // (when there is data to be read on the socket), and perform the following
+    // actions on this kevent: EV_ADD and EV_ENABLE (add the event to the kqueue 
+    // and enable it).
     EV_SET(change_event, socket_listen_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
 
-    // Register event with the kqueue
+    // Register kevent with the kqueue.
     if (kevent(kq, change_event, 1, NULL, 0, NULL) == -1)
     {
         perror("kevent");
         exit(1);
     }
 
-    // Actual event loop
+    // Actual event loop.
     for (;;)
     {
         // Check for new events, but do not register new events with
@@ -73,31 +77,34 @@ int main()
         for (int i = 0; new_events > i; i++)
         {
             int event_fd = event[i].ident;
+
+            // When the client disconnects an EOF is send. By closing the file
+            // descriptor the event is automatically removed from the kqueue.
             if (event[i].flags & EV_EOF)
             {
                 printf("Client has disconnected");
                 close(event_fd);
             }
-            // if the event came from the listening socket, we are sure
-            // that a new client want to connect to our socket
+            // If the new event's file descriptor is the same as the listening
+            // socket's file descriptor, we are sure that a new client want 
+            // to connect to our socket.
             else if (event_fd == socket_listen_fd)
             {
-                // Incoming socket connection on the *listening socket*,
-                // Create a __new__ fd for the actual connection to client
+                // Incoming socket connection on the listening socket.
+                // Create a new socket for the actual connection to client.
                 socket_connection_fd = accept(event_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
                 if (socket_connection_fd == -1)
                 {
                     perror("Accept socket error");
-                    close(event_fd);
                 }
 
                 // Put this new socket connection also as a 'filter' event
-                // to watch in kqueue
+                // to watch in kqueue, so we can now watch for events on this
+                // new socket.
                 EV_SET(change_event, socket_connection_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
                 if (kevent(kq, change_event, 1, NULL, 0, NULL) < 0)
                 {
                     perror("kevent error");
-                    close(event_fd);
                 }
             }
 
